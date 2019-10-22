300894882f652c818a23762dd818c98706a19d4d
Aryan Kharbanda <kharbanda.aryan00@gmail.com>
2019-10-22 19:05:48 +0530

    updated code and added comments

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void main()
 {   
-   printf("hello world!"); //printf is a command to print output on the terminal the statement inside "" is printed.
+   printf("\nHello World!\n"); 
+   //printf is a command to print output on the terminal the statement inside "" is printed.
+   // "\n" is used to move to the next line 
    return 0; 
 }
